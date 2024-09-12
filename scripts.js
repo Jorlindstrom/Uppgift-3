47a0768a7a1da940b7712be64bd85b5c8e132532
Jorlindstrom <jorlind@telia.com>
2024-09-12 12:43:31 +0200

    changed javascript from document.getElementById() to document.querySelector()

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
 function goodbye() {
-    document.getElementById("title").innerHTML = "Goodbye World";
-  }
+    //document.getElementById("title").innerHTML = "Goodbye World";
+    let changeText = document.querySelector("h1");
+    changeText.innerHTML = "Goodbye World";
+    }
